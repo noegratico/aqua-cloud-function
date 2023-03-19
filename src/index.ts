6999122e43a028c4539fa13087cc779652274d97
levigratico <levigratico@gmail.com>
2023-03-19 14:19:17 +0800

    add create user function

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,24 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-// import {DataSnapshot} from "firebase-admin/database";
-// import {Change, EventContext} from "firebase-functions/v1";
-import {UserRecord} from "firebase-admin/auth";
-import {addUserLevelToClaims, addUserToFireStore} from "./user-functions/account";
+import {addUserLevelToClaims, registerUser, User} from "./user-functions/account";
 import {AuthUserRecord} from "firebase-functions/lib/common/providers/identity";
-// import {addSensorDataToDatabase} from "./realtime-data-functions/sensor";
+import {CallableContext} from "firebase-functions/v1/https";
 
 admin.initializeApp();
 
 const firestore = admin.firestore();
 
-const signUp = functions.auth.user().onCreate((user: UserRecord) => {
-  addUserToFireStore(firestore, user);
-});
+// const signUp = functions.auth.user().onCreate((user: UserRecord) => {
+//   addUserToFireStore(firestore, user);
+// });
 
 const beforeSignIn = functions.auth.user().beforeSignIn((user: AuthUserRecord) => {
   addUserLevelToClaims(firestore, user);
 });
 
-// const realtimeSensorData = functions.database.ref("/sensors").onWrite((snap: Change<DataSnapshot>, contex: EventContext) => {
-//   addSensorDataToDatabase(firestore, snap, contex);
-// });
+const httpFunctions = functions.https.onCall((data: User, context: CallableContext) => {
+  registerUser(firestore, data, context);
+});
 
-export {signUp, beforeSignIn};
+
+export {beforeSignIn, httpFunctions};
--- src/user-functions/account.ts
+++ src/user-functions/account.ts
@@ -1,30 +1,35 @@
 import {firestore, auth} from "firebase-admin";
 import {logger} from "firebase-functions";
 import {UserRecord} from "firebase-admin/auth";
-import {WriteResult} from "firebase-admin/firestore";
 import {AuthUserRecord} from "firebase-functions/lib/common/providers/identity";
+import {CallableContext} from "firebase-functions/v1/https";
+import * as functions from "firebase-functions";
 
-interface User {
-  email: string,
-  userLevel: string
+export interface User {
+  id?: string,
+  email?: string,
+  userLevel: string,
+  name: string,
+  password?: string,
 }
 
-/**
- * Creates a record to owned database
- * @param {firestore.Firestore} firestore - firestore instance
- * @param {UserRecord} user - user created during signup
- */
-export async function addUserToFireStore(firestore: firestore.Firestore, user: UserRecord) {
-  const result: WriteResult = await firestore.collection("users").doc(user.uid).set({
-    isActive: user.emailVerified,
-    email: user.email,
-  });
-  logger.write({message: "User inserted", severity: "INFO", writeTime: result.writeTime.toDate()});
-}
+// /**
+//  * Creates a record to owned database
+//  * @param {firestore.Firestore} firestore - firestore instance
+//  * @param {UserRecord} user - user created during signup
+//  */
+// export async function addUserToFireStore(firestore: firestore.Firestore, user: UserRecord) {
+//   const result: WriteResult = await firestore.collection("users").doc(user.uid).set({
+//     isActive: user.emailVerified,
+//     email: user.email,
+//   });
+//   logger.write({message: "User inserted", severity: "INFO", writeTime: result.writeTime.toDate()});
+// }
 
 /**
  * @param {firestore.Firestore} firestore - firestore instance
  * @param {AuthUserRecord} user - verified user object
+ * @return {Promise<void>}
  */
 export async function addUserLevelToClaims(firestore: firestore.Firestore, user: AuthUserRecord): Promise<void> {
   const userRef = firestore.collection("users").doc(user.uid);
@@ -38,4 +43,28 @@ export async function addUserLevelToClaims(firestore: firestore.Firestore, user:
   } else {
     // TODO - insert to firestore as user
   }
+}
+
+/**
+ *
+ * @param {firestore.Firestore} firestore
+ * @param {User} data
+ * @param {CallableContext} context
+ * @return {Promise<User>}
+ */
+export async function registerUser( firestore: firestore.Firestore, data: User, context: CallableContext): Promise<User> {
+  if (context.auth?.token.admin) {
+    const user:UserRecord = await auth().createUser({email: data.email, password: data.password});
+    await firestore.collection("users").doc(user.uid).set({
+      name: data.name,
+      userLevel: data.userLevel,
+    });
+    return {
+      id: user.uid,
+      email: user.email,
+      userLevel: data.userLevel,
+      name: data.name,
+    };
+  }
+  throw new functions.https.HttpsError("permission-denied", "Admin only access!");
 }
